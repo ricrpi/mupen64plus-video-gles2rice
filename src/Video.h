e83a498bb247bd607b35388c7d5bb107d7097688
gizmo98 <atz.SE@gmx.de>
2015-04-05 09:08:05 +0200

    Update WindowSettingsStruct

--- src/Video.h
+++ src/Video.h
@@ -30,7 +30,8 @@ typedef struct {
     float   fViWidth, fViHeight;
     unsigned short        uViWidth, uViHeight;
     unsigned short        uDisplayWidth, uDisplayHeight;
-    
+	unsigned short		  uDisplayX, uDisplayY;
+    BOOL	bDisplayRatio;    
     BOOL    bDisplayFullscreen;
     BOOL    bVerticalSync;
 
